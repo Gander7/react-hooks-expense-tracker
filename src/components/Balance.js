c70b85c3559d7ba459bc513e81a50eff00ed6ef1
Zach <zach@basebridge.com>
2020-02-23 11:19:17 -0400

    init w no logic

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Balance = () => {
+     return (
+         <>
+            <h1>Balance</h1>
+            <h4>$0.00</h4>
+         </>
+     )
+}
+
+export default Balance
