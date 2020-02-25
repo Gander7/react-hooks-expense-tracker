b80b7a6cb8a626c1d9dcf6018e32174a8f27108d
Zach <zach@basebridge.com>
2020-02-25 11:49:49 -0400

    removed header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
@@ -13,7 +12,6 @@ import './App.css';
 function App() {
   return (
     <GlobalProvider>
-      <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
