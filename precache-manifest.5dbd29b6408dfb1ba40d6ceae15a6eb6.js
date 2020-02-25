a0451d9197a8f915ad2a26f0397e87bae6b66ecd
Zach <zach@basebridge.com>
2020-02-25 11:18:08 -0400

    Updates

---
+++ precache-manifest.5dbd29b6408dfb1ba40d6ceae15a6eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d396117ecaa92f21ebced29e724d9ce",
+    "url": "/react-hooks-expense-tracker/index.html"
+  },
+  {
+    "revision": "57eae616842373f63573",
+    "url": "/react-hooks-expense-tracker/static/css/main.8b0f3120.chunk.css"
+  },
+  {
+    "revision": "69c633125c3681c3c796",
+    "url": "/react-hooks-expense-tracker/static/js/2.48cf8f4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-expense-tracker/static/js/2.48cf8f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57eae616842373f63573",
+    "url": "/react-hooks-expense-tracker/static/js/main.a9e1c69f.chunk.js"
+  },
+  {
+    "revision": "0d7c214a097ec0c04b27",
+    "url": "/react-hooks-expense-tracker/static/js/runtime-main.a4e89176.js"
+  }
+]);
